770726bcf28313f82072563a88efa285e94e1315
歪脖子 <37356244+rean0522@users.noreply.github.com>
2022-05-13 20:43:22 +0800

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -58,5 +58,5 @@ svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 # ------------------------------- Other ends -------------------------------
 
 # Add OpenClash
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-amlogic
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
